478ba353164e8a0052e0405d0ed3315530ffee8f
chenziming <basuhqqk@qq.com>
2018-01-31 18:14:27 +0800

    purchase

---
+++ Code/web/dms_part/com.hsweb.part.purchase/src/webcontent/js/allotMgr/allotEnter.js
@@ -0,0 +1,446 @@
+/**
+ * Created by Administrator on 2018/1/31.
+ */
+var baseUrl = window._rootUrl||"http://127.0.0.1:8080/default/";
+var leftGridUrl = baseUrl+"com.hsapi.part.purchase.svr.queryPtsEnterMainList.biz.ext";
+var rightGridUrl = baseUrl+"com.hsapi.part.purchase.svr.queryPtsEnterDetailByEnterId.biz.ext";
+var advancedSearchWin = null;
+var advancedSearchForm = null;
+var advancedSearchFormData = null;
+var basicInfoForm = null;
+var leftGrid = null;
+var rightGrid = null;
+
+
+$(document).ready(function(v)
+{
+    leftGrid = nui.get("leftGrid");
+    leftGrid.setUrl(leftGridUrl);
+
+    rightGrid = nui.get("rightGrid");
+    rightGrid.setUrl(rightGridUrl);
+    advancedSearchWin = nui.get("advancedSearchWin");
+    advancedSearchForm = new nui.Form("#advancedSearchWin");
+    basicInfoForm = new nui.Form("#basicInfoForm");
+    //console.log("xxx");
+
+    getStorehouse(function(data)
+    {
+        var storehouse = data.storehouse||[];
+        nui.get("storeId").setData(storehouse);
+        doSearch({});
+    });
+
+});
+function onLeftGridRowDblClick(e)
+{
+    var row = e.record;
+    if(!row)
+    {
+        row = leftGrid.getSelected();
+    }
+    if(!row)
+    {
+        return;
+    }
+    //   console.log(row);
+    basicInfoForm.setData(row);
+    nui.get("guestId").setText(row.guestFullName);
+    var editEnterMainBtn = nui.get("editEnterMainBtn");
+    var saveEnterMainBtn = nui.get("saveEnterMainBtn");
+    var cancelEditEnterMainBtn = nui.get("cancelEditEnterMainBtn");
+    var reViewBtn = nui.get("reViewBtn");
+    cancelEditEnterMainBtn.disable();
+    saveEnterMainBtn.disable();
+    var addPartBtn = nui.get("addPartBtn");
+    var editPartBtn = nui.get("editPartBtn");
+    var deletePartBtn = nui.get("deletePartBtn");
+
+    addPartBtn.disable();
+    editPartBtn.disable();
+    deletePartBtn.disable();
+    if(row.auditStatus == 0)
+    {
+        editEnterMainBtn.enable();
+        reViewBtn.enable();
+
+    }
+    else{
+        editEnterMainBtn.disable();
+        reViewBtn.disable();
+    }
+    var enterId = row.id;
+    loadRightGridData(enterId);
+}
+function loadRightGridData(enterId)
+{
+    editPartHash={};
+    rightGrid.load({
+        enterId:enterId
+    });
+}
+function quickSearch(type){
+    var params = {};
+    switch (type)
+    {
+        case 0:
+            params.today = 1;
+            break;
+        case 1:
+            params.yesterday = 1;
+            break;
+        case 2:
+            params.thisWeek = 1;
+            break;
+        case 3:
+            params.lastWeek = 1;
+            break;
+        case 4:
+            params.thisMonth = 1;
+            break;
+        case 5:
+            params.lastMonth = 1;
+            break;
+        case 6:
+            params.auditStatus = 0;
+            break;
+        case 7:
+            params.auditStatus = 1;
+            break;
+        case 8:
+            params.postStatus = 1;
+            break;
+        default:
+            break;
+    }
+    doSearch(params);
+}
+function onSearch(){
+    search();
+}
+function search()
+{
+    var param = getSearchParam();
+    doSearch(param);
+}
+function getSearchParam(){
+    var params = {};
+    params.guestId = nui.get("searchGuestId").getValue();
+    return params;
+}
+function doSearch(params)
+{
+    params.enterTypeId = '050106';//调拨入库
+    leftGrid.load({
+        params:params
+    },function(){
+        onLeftGridRowDblClick({});
+    });
+}
+function advancedSearch()
+{
+    advancedSearchWin.show();
+    advancedSearchForm.clear();
+    if(advancedSearchFormData)
+    {
+        advancedSearchForm.setData(advancedSearchFormData);
+    }
+}
+function onAdvancedSearchOk()
+{
+    var searchData = advancedSearchForm.getData();
+    advancedSearchWin.hide();
+    doSearch(searchData);
+}
+function onAdvancedSearchCancel(){
+    advancedSearchForm.clear();
+    advancedSearchWin.hide();
+}
+function addInbound()
+{
+    basicInfoForm.clear();
+    var data = {
+        enterDate:(new Date()),
+        totalAmt:0
+    };
+    basicInfoForm.setData(data);
+    rightGrid.clearRows();
+    var editEnterMainBtn = nui.get("editEnterMainBtn");
+    var saveEnterMainBtn = nui.get("saveEnterMainBtn");
+    var cancelEditEnterMainBtn = nui.get("cancelEditEnterMainBtn");
+    var reViewBtn = nui.get("reViewBtn");
+    reViewBtn.disable();
+    editEnterMainBtn.disable();
+    cancelEditEnterMainBtn.enable();
+    saveEnterMainBtn.enable();
+}
+function editInbound(){
+    var editEnterMainBtn = nui.get("editEnterMainBtn");
+    var saveEnterMainBtn = nui.get("saveEnterMainBtn");
+    var cancelEditEnterMainBtn = nui.get("cancelEditEnterMainBtn");
+    var reViewBtn = nui.get("reViewBtn");
+    reViewBtn.disable();
+    editEnterMainBtn.disable();
+    cancelEditEnterMainBtn.enable();
+    saveEnterMainBtn.enable();
+
+    var addPartBtn = nui.get("addPartBtn");
+    var editPartBtn = nui.get("editPartBtn");
+    var deletePartBtn = nui.get("deletePartBtn");
+    addPartBtn.enable();
+    editPartBtn.enable();
+    deletePartBtn.enable();
+}
+function cancelEditInbound(){
+
+    nui.confirm("确认取消吗？","提示",function(action)
+    {
+        if(action == "ok")
+        {
+            onLeftGridRowDblClick({});
+        }
+    });
+}
+var requiredField = {
+    storeId:"仓库",
+    guestId:"供应商",
+    billTypeId:"票据类型"
+};
+var saveUrl = baseUrl+"com.hsapi.part.purchase.crud.savePtsEnter.biz.ext";
+function save()
+{
+    var data = basicInfoForm.getData();
+    for(var key in requiredField)
+    {
+        if(!data[key] || data[key].trim().length==0)
+        {
+            nui.alert(requiredField[key]+"不能为空");
+            return;
+        }
+    }
+    if(supplier)
+    {
+        data.guestFullName = supplier.fullName;
+    }
+    data.enterTotalQty = 0;
+    data.taxAmt = 0;
+    data.goodsAmt = 0;
+    data.payableAmt = 0;
+    console.log(data);
+    var enterDetailAdd = rightGrid.getChanges("added")||[];
+    var enterDetailUpdate = [];
+    for(var key in editPartHash)
+    {
+        if(typeof editPartHash[key] == 'object')
+        {
+            enterDetailUpdate.push(editPartHash[key]);
+        }
+    }
+    var enterDetailDelete = rightGrid.getChanges("removed")||[];
+    enterDetailDelete = enterDetailDelete.filter(function(v)
+    {
+        return v.detailId;
+    });
+    nui.mask({
+        html:'保存中...'
+    });
+    nui.ajax({
+        url:saveUrl,
+        type:"post",
+        data:JSON.stringify({
+            enterMain:data,
+            enterDetailAdd:enterDetailAdd,
+            enterDetailUpdate:enterDetailUpdate,
+            enterDetailDelete:enterDetailDelete
+        }),
+        success:function(data)
+        {
+            nui.unmask();
+            data = data||{};
+            if(data.errCode == "S")
+            {
+                nui.alert("保存成功");
+                onLeftGridRowDblClick({});
+            }
+            else{
+                nui.alert(data.errMsg||"保存失败");
+            }
+        },
+        error:function(jqXHR, textStatus, errorThrown){
+            //  nui.alert(jqXHR.responseText);
+            console.log(jqXHR.responseText);
+        }
+    });
+}
+var supplier = null;
+function selectSupplier(elId)
+{
+    supplier = null;
+    nui.open({
+        targetWindow: window,
+        url: "../common/supplierSelectView.html",
+        title: "供应商资料", width: 980, height: 560,
+        allowDrag:true,
+        allowResize:true,
+        onload: function ()
+        {
+
+        },
+        ondestroy: function (action)
+        {
+            if(action == 'ok')
+            {
+                var iframe = this.getIFrameEl();
+                var data = iframe.contentWindow.getData();
+                console.log(data);
+                console.log(elId);
+                supplier = data.supplier;
+                var value = supplier.id;
+                var text = supplier.fullName;
+                var el = nui.get(elId);
+                el.setValue(value);
+                el.setText(text);
+            }
+        }
+    });
+}
+
+function selectPart(callback)
+{
+    nui.open({
+        targetWindow: window,
+        url: "../common/partSelectView.html",
+        title: "供应商资料", width: 930, height: 560,
+        allowDrag:true,
+        allowResize:true,
+        onload: function ()
+        {
+            var iframe = this.getIFrameEl();
+            iframe.contentWindow.setData({},callback);
+        },
+        ondestroy: function (action)
+        {
+        }
+    });
+}
+function addEnterDetail(part)
+{
+    nui.open({
+        targetWindow: window,
+        url: "./inBoundCountView.html",
+        title: "入库数量金额", width: 430, height:210,
+        allowDrag:true,
+        allowResize:false,
+        onload: function ()
+        {
+            var iframe = this.getIFrameEl();
+            part.partId = part.id;
+            delete part.id;
+            part.partCode = part.code;
+            part.partName = part.fullName;
+            part.noTaxUnitPrice = 0;
+            iframe.contentWindow.setData({
+                part:part
+            });
+        },
+        ondestroy: function (action)
+        {
+            if(action == "ok")
+            {
+                var iframe = this.getIFrameEl();
+                var data = iframe.contentWindow.getData();
+                var enterDetail = data.enterDetail;
+                console.log(enterDetail);
+                rightGrid.addRow(enterDetail);
+            }
+        }
+    });
+}
+
+function addPart()
+{
+    selectPart(function(data)
+    {
+        var part = data.part;
+        console.log(part);
+        addEnterDetail(part);
+    });
+}
+var editPartHash = {
+};
+function editPart()
+{
+    var part = rightGrid.getSelected();
+    if(!part)
+    {
+        return;
+    }
+    nui.open({
+        targetWindow: window,
+        url: "./enterDetailEditView.html",
+        title: "数量金额", width: 430, height:210,
+        allowDrag:true,
+        allowResize:false,
+        onload: function ()
+        {
+            var iframe = this.getIFrameEl();
+            iframe.contentWindow.setData({
+                part:part
+            });
+        },
+        ondestroy: function (action)
+        {
+            if(action == "ok")
+            {
+                var iframe = this.getIFrameEl();
+                var data = iframe.contentWindow.getData();
+                var enterDetail = data.enterDetail;
+                var billTypeId = nui.get("billTypeId").getValue();
+                console.log(enterDetail);
+                //       rightGrid.addRow(enterDetail);
+                enterDetail.taxSign = 0;
+                enterDetail.taxRate = 0.07;
+                if(billTypeId == "010103")
+                {
+                    enterDetail.taxRate = 0.17;
+                }
+                var totalTaxRate = 1+enterDetail.taxRate;
+                enterDetail.taxUnitPrice = (totalTaxRate*enterDetail.noTaxUnitPrice).toFixed(2);//含税单价=税率*
+                enterDetail.taxAmt = (enterDetail.taxUnitPrice*enterDetail.enterQty).toFixed(2);//含税总额
+                enterDetail.noTaxAmt = (enterDetail.noTaxUnitPrice*enterDetail.enterQty).toFixed(2);//不含税总额
+                enterDetail.taxRateAmt = (enterDetail.taxAmt-enterDetail.noTaxAmt).toFixed(2);//税额
+                enterDetail.outableQty = enterDetail.enterQty;
+                enterDetail.suggestPrice = 0;
+                enterDetail.suggestAmt = 0;
+
+                part.taxSign = enterDetail.taxSign;
+                part.taxRate = enterDetail.taxRate;
+                part.taxUnitPrice = enterDetail.taxUnitPrice;
+                part.taxAmt = enterDetail.taxAmt;
+                part.noTaxAmt = enterDetail.noTaxAmt;
+                part.outableQty = enterDetail.outableQty;
+                part.enterQty = enterDetail.enterQty;
+                part.noTaxUnitPrice = enterDetail.noTaxUnitPrice;
+                part.suggestAmt = enterDetail.suggestAmt;
+                part.suggestPrice = enterDetail.suggestPrice;
+                rightGrid.updateRow(part,part);
+                if(part.detailId && !editPartHash[part.detailId])
+                {
+                    editPartHash[part.detailId] = part;
+                }
+            }
+        }
+    });
+}
+function deletePart(){
+    var part = rightGrid.getSelected();
+    if(!part)
+    {
+        return;
+    }
+    if(part.detailId && editPartHash[part.detailId])
+    {
+        delete editPartHash[part.detailId];
+    }
+    rightGrid.removeRow(part,true);
+}
---
+++ Code/web/dms_part/com.hsweb.part.purchase/src/webcontent/stockQuery_view0.jsp
@@ -0,0 +1,165 @@
+<%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
+<%@include file="/common/common.jsp"%>
+<%@include file="/common/commonPart.jsp"%>
+<html>
+<!-- 
+  - Author(s): chenziming
+  - Date: 2018-01-31 16:41:54
+  - Description:
+-->
+<head>
+<title>库存查询</title>
+<script src="<%= request.getContextPath() %>/purchase/js/stockMgr/stockQuery.js?v=1.0.1"></script>
+<style type="text/css">
+.table-label {
+	text-align: right;
+}
+</style>
+</head>
+<body>
+
+<div class="nui-toolbar" style="padding:2px;border-bottom:0;">
+    <div class="form" id="queryForm">
+        <table style="width:100%;">
+            <tr>
+                <td style="white-space:nowrap;">
+                    <label style="font-family:Verdana;">快速查询：</label>
+                    <label style="font-family:Verdana;">分店名称：</label>
+                    <input id="compCode"
+                           name="compCode"
+                           class="nui-combobox width1"
+                           textField="text"
+                           valueField="id"
+                           emptyText="请选择..."
+                           url=""
+                           allowInput="true"
+                           showNullItem="true"
+                           nullItemText="请选择..."/>
+                    <label style="font-family:Verdana;">仓库：</label>
+                    <input id="storeId"
+                           name="storeId"
+                           class="nui-combobox width1"
+                           textField="name"
+                           valueField="id"
+                           emptyText="请选择..."
+                           url=""
+                           allowInput="true"
+                           showNullItem="false"
+                           nullItemText="请选择..."/>
+                    <label style="font-family:Verdana;">配件编码：</label>
+                    <input class="nui-textbox" width="100" name="partCode"/>
+                    <label style="font-family:Verdana;">名称：</label>
+                    <input class="nui-textbox" width="100" name="name"/>
+                    <label style="font-family:Verdana;">仓位：</label>
+                    <input class="nui-textbox" width="100" name="storeLocation"/>
+                    <label style="font-family:Verdana;">车型：</label>
+                    <input class="nui-textbox" width="100" name="carType"/>
+                    <label style="font-family:Verdana;">品牌：</label>
+                    <input id="carBrand"
+                           name="carBrand"
+                           class="nui-combobox width1"
+                           textField="carBrandZh"
+                           valueField="id"
+                           emptyText="请选择..."
+                           url=""
+                           allowInput="true"
+                           showNullItem="false"
+                           nullItemText="请选择..."/>
+                    <span class="separator"></span>
+                    <a class="nui-button" iconCls="icon-search" plain="true" onclick="onSearch()">查询</a>
+                    <!--<a class="nui-button" plain="true" onclick="advancedSearch()">更多</a>-->
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class="nui-fit">
+    <div class="nui-splitter"
+         allowResize="false"
+         style="width:100%;height:100%;">
+        <div size="210" showCollapseButton="false">
+            <div class="nui-toolbar" style="padding:2px;border-top:0;border-left:0;border-right:0;text-align: center;">
+                <span>系统分类</span>
+            </div>
+            <div class="nui-fit">
+                <ul id="tree1" class="nui-tree" url="" style="width:100%;"
+                    dataField="partTypes"
+                    ondrawnode="onDrawNode"
+                    onnodedblclick="onNodeDblClick"
+                    showTreeIcon="true" textField="name" idField="id" parentField="parentid" resultAsTree="false">
+                </ul>
+            </div>
+        </div>
+        <div showCollapseButton="false">
+            <div class="nui-toolbar" style="padding:2px;border-top:0;border-left:0;border-right:0;">
+                <span>零件列表</span>
+            </div>
+            <div class="nui-fit" >
+                <div id="partGrid" class="nui-datagrid" style="width:100%;height:100%;"
+                     frozenStartColumn="0"
+                     frozenEndColumn="7"
+                     borderStyle="border:0;"
+                     dataField="parts"
+                     url=""
+                     ondrawcell="onPartGridDraw"
+                     idField="id"
+                     totalField="page.count"
+                     selectOnLoad="true"
+                     pageSize="50"
+                     showFilterRow="false" allowCellSelect="true" allowCellEdit="false">
+                    <div property="columns">
+                        <div type="indexcolumn">序号</div>
+                        <div header="分店仓库信息" headerAlign="center">
+                            <div property="columns">
+                                <div field="compName" width="50" headerAlign="center" header="分店名称"></div>
+                                <div field="storeId" width="50" headerAlign="center" header="仓库"></div>
+                                <div field="storeLocationName" width="50" headerAlign="center" header="仓位"></div>
+                            </div>
+                        </div>
+                        <div header="配件信息" headerAlign="center">
+                            <div property="columns">
+                                <div field="partId" width="50" headerAlign="center" header="内码"></div>
+                                <div field="partCode" width="80" headerAlign="center" allowSort="true" header="配件编码"></div>
+                                <div field="partName" width="80" headerAlign="center" allowSort="true" header="配件名称"></div>
+                                <div field="partFullName" width="80" headerAlign="center" allowSort="true" header="配件全称"></div>
+                                <div field="partQualityId" width="80" headerAlign="center" allowSort="true" header="品质"></div>
+                                <div field="partBrandId" width="80" headerAlign="center" allowSort="true" header="品牌"></div>
+                                <div field="abcType" width="80" headerAlign="center" allowSort="true" header="ABC分类"></div>
+                            </div>
+                        </div>
+                        <div header="库存信息" headerAlign="center">
+                            <div property="columns">
+                                <div field="outAbleQty" width="50" headerAlign="center" header="数量"></div>
+                                <div field="unit" width="80" headerAlign="center" allowSort="true" header="单位"></div>
+                                <div field="noTaxUnitPrice" width="80" headerAlign="center" allowSort="true" header="单价"></div>
+                                <div field="noTaxAmt" width="80" headerAlign="center" allowSort="true" header="金额"></div>
+                                <div field="suggestPrice" width="80" headerAlign="center" allowSort="true" header="建议销价"></div>
+                                <div field="sellPrice" width="80" headerAlign="center" allowSort="true" header="最后销价"></div>
+                            </div>
+                        </div>
+                        <div header="上下限" headerAlign="center">
+                            <div property="columns">
+                                <div field="heightLimit" width="60" headerAlign="center" align="right" allowSort="true" header="上限"></div>
+                                <div field="lowLimit" width="60" headerAlign="center" align="right" allowSort="true" header="下限"></div>
+                            </div>
+                        </div>
+                        <div header="其他信息" headerAlign="center">
+                            <div property="columns">
+                                <div field="applyCarModel" width="60" headerAlign="center" allowSort="true" header="车型"></div>
+                                <div field="carTypeIdF" width="60" headerAlign="center" allowSort="true" header="一级分类"></div>
+                                <div field="carTypeIdS" width="60" headerAlign="center" allowSort="true" header="二级分类"></div>
+                                <div field="carTypeIdT" width="60" headerAlign="center" allowSort="true" header="三级分类"></div>
+                                <div field="lastEnyterDate" dateFormat="yyyy-MM-dd H:mm:ss" width="60" headerAlign="center" allowSort="true" header="最近入库日期"></div>
+                                <div field="lastOutDate" dateFormat="yyyy-MM-dd H:mm:ss" width="60" headerAlign="center" allowSort="true" header="最近出库日期"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>

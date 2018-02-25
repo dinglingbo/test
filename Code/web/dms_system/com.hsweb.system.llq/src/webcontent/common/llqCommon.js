d14e9be0c775011c34bc1a6f2b30bcb64f0f48ff
guine <44971453@qq.com>
2018-02-25 17:25:48 +0800

    vin查询主组数据及车辆信息

--- Code/web/dms_system/com.hsweb.system.llq/src/webcontent/common/llqCommon.js
+++ Code/web/dms_system/com.hsweb.system.llq/src/webcontent/common/llqCommon.js
@@ -3,7 +3,8 @@
 var url = "com.hsweb.system.llq.call.doCall.biz.ext";
 
 function loadData(url, params, callBack){
-	nui.ajax({
+	callAjax(url, params, processRs);
+    nui.ajax({
 		url: url,
 		type: "post",
 		cache: false,
@@ -22,4 +23,35 @@ function loadData(url, params, callBack){
 			nui.alert("获取数据遇到错误！");
 		}
 	});
+}
+
+/*
+* 执行Ajax
+*/
+function callAjax(url, params, processAjax, callBack){
+	nui.ajax({
+		url: url,
+		type: "post",
+        data: params,
+		contentType: 'text/json',
+		success: function (json) {
+            processAjax(json, callBack);
+		},
+		error: function () {
+			nui.alert("获取数据遇到错误！");
+		}
+	});
+}
+
+/*
+* 处理Ajax结果
+*/
+function processAjax(rs, callBack){
+    if(rs.result.code == '1'){
+        //nui.alert("获取数据成功！");
+        callBack(rs.result.data);
+    }
+    else{
+        nui.alert("获取数据失败！");
+    }
 }

cb6c1065117cc4e3d14044f286b111e6048e0f78
guine <44971453@qq.com>
2018-03-23 16:08:36 +0800

    007vin_web

---
+++ DOC/零零汽/007vin_web/007vin/js/store.js
@@ -0,0 +1,12 @@
+// 定义全局数据
+define(function() {
+  'use strict';
+  return {
+    search: null,
+    carInfo: null,
+    carAttrGroup: null,
+    carAttrSubgroup: null,
+    carPartsText: null,
+    carPartsImg: null
+  }
+})

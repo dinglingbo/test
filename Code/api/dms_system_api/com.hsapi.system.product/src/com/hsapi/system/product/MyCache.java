3b315b6ce1dba4eb7422486ab5a567e8b60d2b59
guine <44971453@qq.com>
2018-03-28 16:14:04 +0800

    缓存测试

---
+++ Code/api/dms_system_api/com.hsapi.system.product/src/com/hsapi/system/product/MyCache.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.hsapi.system.product;
+
+import com.eos.system.annotation.Bizlet;
+
+/**
+ * @author Guine
+ * @date 2018-03-28 15:07:42
+ * 
+ */
+@Bizlet("")
+public class MyCache {
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

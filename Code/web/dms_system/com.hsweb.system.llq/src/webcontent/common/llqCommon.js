e9537843219f739454421564ecea6dba1ca452cd
guine <44971453@qq.com>
2018-04-13 18:56:08 +0800

    保险公司

--- Code/web/dms_system/com.hsweb.system.llq/src/webcontent/common/llqCommon.js
+++ Code/web/dms_system/com.hsweb.system.llq/src/webcontent/common/llqCommon.js
@@ -1,6 +1,7 @@
 //http://14.23.35.20:6288/dms/com.hsweb.system.llq.call.doCall.biz.ext
 
 var url = "com.hsweb.system.llq.call.doCall.biz.ext";
+url = "http://g-part.cn:8070/api/sign007";
 
 function loadData(url, params, callBack){
 	callAjax(url, params, processRs);
@@ -29,13 +30,26 @@ function loadData(url, params, callBack){
 * 执行Ajax
 */
 function callAjax(url, params, processAjax, callBack){
-	nui.ajax({
+    //参数匹配
+    var p = {}
+    p.url = params.url.replace("//llq.", "//llqapitm.")
+    p.data = [];
+    p.data.push(params.params);
+    /*for(var k in obj){//用javascript的for/in循环遍历对象的属性 
+        //temp += i+":"+obj[i]+"\n"; 
+        alert(k + '=' + obj[k]);
+        //params.data.push({k:obj[k]});
+    }
+    //return;*/
+    
+    nui.ajax({
 		url: url,
-		type: "post",
-        data: params,
-		contentType: 'text/json',
+		type: "POST",
+        data: JSON.stringify(p),
+		contentType: 'application/json;charset=utf-8',
+        dataType:"json",
 		success: function (json) {
-            processAjax(json, callBack);
+            processAjax(json, callBack);            
 		},
 		error: function () {
 			nui.alert("获取数据遇到错误！");
@@ -46,13 +60,23 @@ function callAjax(url, params, processAjax, callBack){
 /*
 * 处理Ajax结果
 */
-function processAjax(rs, callBack){
+function processAjax(data, callBack){
+    /*
     if(rs.errCode != 'E' && rs.result.code == '1'){
         //nui.alert("获取数据成功！");
         callBack(rs.result.data, rs.result);
     }
     else{
         nui.alert("获取数据失败！\n\r[" + (rs.errMsg || rs.result.msg) + "]");
+    }*/
+    var rs = eval("(" + data + ")");
+    //rs = data;
+    if(rs.code == '1'){
+        //nui.alert("获取数据成功！");
+        callBack(rs.data, rs);
+    }
+    else{
+        nui.alert("获取数据失败！\n\r[" + (rs.errMsg || rs.msg) + "]");
     }
 }
 

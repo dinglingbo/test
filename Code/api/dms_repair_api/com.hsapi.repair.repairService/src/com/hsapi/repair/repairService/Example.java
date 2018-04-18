840ec8c197c3631e30ea76b0a4a5ad28a0f8fb05
chenziming <basuhqqk@qq.com>
2018-04-18 15:59:11 +0800

    api repair

---
+++ Code/api/dms_repair_api/com.hsapi.repair.repairService/src/com/hsapi/repair/repairService/Example.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.hsapi.repair.repairService;
+
+import com.eos.system.annotation.Bizlet;
+
+/**
+ * @author chenziming
+ * @date 2018-04-13 10:31:37
+ *
+ */
+@Bizlet("")
+public class Example {
+	private static void testMethod(){
+        System.out.println("testMethod");
+   }
+   public static void main(String[] args) {
+        ((Example)null).testMethod();
+   }
+}

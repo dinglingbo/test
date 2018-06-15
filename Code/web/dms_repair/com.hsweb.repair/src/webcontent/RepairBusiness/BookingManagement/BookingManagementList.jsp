e2fcbaffde4c41650dbf0fa08b9cfb9f68368ad8
632801287 <632801287@qq.com>
2018-06-15 10:50:20 +0800

    Code\api\dms_repair_api\com.hsapi.repair.repairService\src\com\hsapi\repair\repairService\booking.namingsqlx Code\api\dms_system_api\com.hsapi.system.config\src\com\hsapi\system\confi\paramSet\saveBusinessSort.bizx Code\web\dms_repair\com.hsweb.repair\src\webcontent\RepairBusiness\BookingManagement\BookingManagementEdit.jsp Code\web\dms_repair\com.hsweb.repair\src\webcontent\RepairBusiness\BookingManagement\BookingManagementList.jsp Code\web\dms_repair\com.hsweb.repair\src\webcontent\js\RepairBusiness\BookingManagement\BookingManagementList.js Code\web\dms_system\com.hsweb.system.config\src\webcontent\js\businessSort.js Code\web\dms_repair\com.hsweb.repair\src\webcontent\js\RepairBusiness\BookingManagement\BookingManagementEdit.js

--- Code/web/dms_repair/com.hsweb.repair/src/webcontent/RepairBusiness/BookingManagement/BookingManagementList.jsp
+++ Code/web/dms_repair/com.hsweb.repair/src/webcontent/RepairBusiness/BookingManagement/BookingManagementList.jsp
@@ -10,7 +10,7 @@
 -->
 <head>
 <title>预约列表</title>
-<script src="<%=request.getContextPath()%>/repair/js/RepairBusiness/BookingManagement/BookingManagementList.js?v=1.0.31"></script>
+<script src="<%=request.getContextPath()%>/repair/js/RepairBusiness/BookingManagement/BookingManagementList.js?v=1.0.33"></script>
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <style type="text/css">
 table {
@@ -84,7 +84,7 @@ table {
             <td>
             
                 <a class="nui-button" plain="true" id="addBtn" onclick="addRow()">新增</a>
-                <a class="nui-button" plain="true" id="saveBtn" onclick="updataRow()">修改</a>
+                <a class="nui-button" plain="true" id="saveBtn" onclick="save()">修改</a>
                 <a class="nui-button" plain="true" iconCls="" onclick="quote()">确认</a>
                 <a class="nui-button" plain="true" iconCls="" onclick="quote()">开单</a>
                 <a class="nui-button" plain="true"  id="cancelBtn"  onclick="cancel()">取消</a>
@@ -112,7 +112,7 @@ table {
                      allowSortColumn="true">
                     <div property="columns">
                         <div field="id" headerAlign="center" allowSort="true"
-                             visible="true" width="">id
+                             visible="false" width="">id
                         </div>
                         <div field="mtAdvisor" headerAlign="center" allowSort="true"
                              visible="true" width="">服务顾问
@@ -132,7 +132,7 @@ table {
                         <div field="contactorTel" headerAlign="center" allowSort="true"
                              visible="true" width="">联系电话
                         </div>
-                        <div field="bookStatus" headerAlign="center" allowSort="true"
+                        <div field="serviceTypeId" headerAlign="center" allowSort="true"
                              visible="true" width="">业务类型
                         </div>
                         <div field="predictComeDate" headerAlign="center" allowSort="true"
---
+++ Code/web/dms_repair/com.hsweb.repair/src/webcontent/js/RepairBusiness/BookingManagement/BookingManagementEdit.js
@@ -0,0 +1,182 @@
+baseUrl = apiPath + sysApi + "/";;
+var queryForm;
+var basicInfoForm;
+var upGrid;
+var downGrid;
+var currGuest;
+var assignStatus;
+var timeStatus;
+var rOrp;
+var statusStatus;
+var advancedSearchWin = null;
+var listUrl= baseUrl + "com.hsapi.repair.repairService.booking.queryBookingList.biz.ext";
+
+$(document).ready(function(v){
+	
+	
+	getBisinessList(function(data) {
+		var bisinessList=[];
+		bisinessList = data.data;
+		nui.get("serviceTypeId").setData(bisinessList);
+		
+		
+	});
+	
+	
+});
+
+
+
+/*
+ *设置时间菜单
+ **/
+function setMenu(obj, target, value){
+    target.setValue(value);
+    target.setText(obj.getText());   
+   
+}
+
+function setMenu1(obj, target, value){
+    target.setValue(value);
+    target.setText(obj.getText());   
+   
+}
+
+function onOk() {
+
+	var s = GetData();
+	if (s != undefined) {
+		nui.ajax({
+			url : apiPath + frmApi
+					+ "/com.hsapi.frm.setting.updateguarante.biz.ext",
+			type : 'post',
+			data : nui.encode({
+				params : s
+			}),
+			success : function(data) {
+				if (data.errCode == "S") {
+					nui.alert("挂账成功！");
+					closeWindow("ok");
+				} else {
+					closeWindow("ok");
+				}
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				nui.alert(jqXHR.responseText);
+			}
+		});
+	}
+
+}
+function onCancel(){
+	
+	closeWindow("ok");
+}
+
+
+
+function search() {
+    var param = getSearchParam();
+    doSearch(param);
+}
+
+function getSearchParam() {
+	queryForm = new nui.Form("#queryForm");
+    var params = queryForm.getData();
+    return params;
+}
+
+function doSearch(params) {
+	basicInfoForm = new nui.Form("#basicInfoForm");	
+	nui.ajax({
+           url: listUrl,
+           type: 'post',
+           data: nui.encode({
+        	 params: params
+           }),
+           success: function (data) {
+        	   basicInfoForm.setData(data.data[0]);
+           },
+           error: function (jqXHR, textStatus, errorThrown) {
+               nui.alert(jqXHR.responseText);
+           }
+       });}
+	
+function addCarBrand() {
+    selectCustomer(function (car) {
+        var maintain = {
+            guestId: car.guestId,
+            contactorId: car.contactorId,
+            carId: car.id,
+            carNo: car.carNo,
+            carVin: car.underpanNo,
+            serviceTypeId: "0",
+            mtType: "0",
+            mtAdvisor: currUserName,
+            insureCompCode: car.insureCompCode
+        };
+        nui.mask({
+            html: '保存中..'
+        });
+        getServiceCode(function (data) {
+            var code = data.code;
+            if (!code) {
+                nui.unmask();
+                nui.alert("获取单号失败");
+                return;
+            }
+            maintain.serviceCode = code;
+            saveMaintain(maintain, function (data) {
+                nui.unmask();
+                data = data || {};
+                if (data.errCode == "S") {
+                    reload();
+                }
+                else {
+                    nui.alert(data.errMsg || "新增失败");
+                }
+            });
+        });
+    });
+}
+
+function selectCustomer(callback) {
+    nui.open({
+        url: "com.hsweb.RepairBusiness.Customer.flow",
+        title: "客户选择", width: 800, height: 450,
+        onload: function () {
+        },
+        ondestroy: function (action) {
+            if ("ok" == action) {
+                var iframe = this.getIFrameEl();
+                var data = iframe.contentWindow.getData();
+                var guest = data.guest;
+                callback && callback(guest);
+            }
+        }
+    });
+}
+
+
+/*
+ * 
+ * 获取业务分类列表*/
+var bisinessUrl = baseUrl + "com.hsapi.system.confi.paramSet.getbusinessSort.biz.ext";
+function getBisinessList(callback){
+    nui.ajax({
+        url: bisinessUrl,
+        type: 'post',
+        data: nui.encode({
+        }),
+        cache: false,
+        success: function (data) {
+            if (data) {
+             callback(data);
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            nui.alert(jqXHR.responseText);
+        }
+	});
+	
+}

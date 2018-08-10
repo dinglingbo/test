f4f698a559731bedad2591dad28584e5ee8856d0
xiongying <1074331924@qq.com>
2018-08-10 17:41:05 +0800

    套餐明细完善

--- Code/web/dms_repair/com.hsweb.repair/src/webcontent/DataBase/Card/timesCardList.jsp
+++ Code/web/dms_repair/com.hsweb.repair/src/webcontent/DataBase/Card/timesCardList.jsp
@@ -14,67 +14,63 @@
 <script
 	src="<%=request.getContextPath()%>/repair/js/Card/timesCardList.js?v=1.0.3"></script>
 </head>
-<body >
+<body>
 
 	<!--footer-->
 
-	
-		<div class="nui-toolbar" style="border-bottom: 0; padding: 0px;">
-			<div id="queryform" class="nui-form" align="center">
-				<!-- 数据实体的名称 -->
-				<input class="nui-hidden" name="criteria/_entity"
-					value="com.hsapi.repair.data.rpb.RpbCardTimes">
-				<!-- 排序字段 -->
 
-				<table style="width: 100%;" id="table1">
-					<tr>
-						<td style="width: 100%;">计次卡名称: <input class="nui-textbox"
-							name="criteria/_expr[1]/name" /> <input class="nui-hidden"
-							name="criteria/_expr[1]/_op" value="like"> <input
-							class="nui-hidden" name="criteria/_expr[1]/_likeRule" value="all">
-							<a class="nui-button" onclick="search()" plain="true"> 查询 </a> <span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>
-							<a class="nui-button" iconCls="icon-add" onclick="add()"
-							plain="true"> 增加 </a> <a id="update" class="nui-button"
-							iconCls="icon-edit" onclick="edit()" plain="true"> 编辑 </a>
+	<div class="nui-toolbar" style="border-bottom: 0; padding: 0px;">
+		<div id="queryform" class="nui-form" align="center">
+			<!-- 数据实体的名称 -->
+			<input class="nui-hidden" name="criteria/_entity"
+				value="com.hsapi.repair.data.rpb.RpbCardTimes">
+			<!-- 排序字段 -->
 
-						</td>
-					</tr>
-				</table>
+			<table style="width: 100%;" id="table1">
+				<tr>
+					<td style="width: 100%;">计次卡名称: <input class="nui-textbox"
+						name="criteria/_expr[1]/name" /> <input class="nui-hidden"
+						name="criteria/_expr[1]/_op" value="like"> <input
+						class="nui-hidden" name="criteria/_expr[1]/_likeRule" value="all">
+						<a class="nui-button" onclick="search()" plain="true"> 查询 </a> <span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span>
+						<a class="nui-button" iconCls="icon-add" onclick="add()"
+						plain="true"> 增加 </a> <a id="update" class="nui-button"
+						iconCls="icon-edit" onclick="edit()" plain="true"> 编辑 </a>
+
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<div class="nui-fit">
+		<div id="datagrid1" dataField="timesCard" class="nui-datagrid"
+			style="width: 100%; height: 100%;" pageSize="20" showPageInfo="true"
+			onDrawCell="onDrawCell" onselectionchanged="selectionChanged"
+			allowSortColumn="false">
+			<div property="columns">
+				<div type="indexcolumn"></div>
+				<div type="checkcolumn" class="mini-radiobutton"></div>
+				<div field="id" headerAlign="center" allowSort="true"
+					visible="false">计次卡ID</div>
+				<div field="name" headerAlign="center" allowSort="true">计次卡名称</div>
+				<div field="periodValidity" headerAlign="center" allowSort="true">
+					有效期(月)</div>
+				<div field="sellAmt" headerAlign="center" allowSort="true">
+					销售价格</div>
+				<div field="totalAmt" headerAlign="center" allowSort="true">
+					总价值</div>
+
+				<div field="salesDeductType" headerAlign="center" allowSort="true">销售提成方式</div>
+				<div field="salesDeductValue" headerAlign="center" allowSort="true">
+					销售提成值</div>
+				<div field="status" headerAlign="center" allowSort="true">状态</div>
+				<div field="useRemark" headerAlign="center" allowSort="true">
+					使用说明</div>
+				<div field="remark" headerAlign="center" allowSort="true">卡说明</div>
 			</div>
 		</div>
-		<div class="nui-fit">
-			<div id="datagrid1" dataField="timesCard" class="nui-datagrid"
-				style="width: 100%;height: 100%;" pageSize="20" showPageInfo="true" onDrawCell="onDrawCell"
-				 onselectionchanged="selectionChanged"
-				allowSortColumn="false">
-				<div property="columns">
-					<div type="indexcolumn"></div>
-					<div type="checkcolumn" class="mini-radiobutton"></div>
-					<div field="id" headerAlign="center" allowSort="true"
-						visible="false">计次卡ID</div>
-					<div field="name" headerAlign="center" allowSort="true">
-						计次卡名称</div>
-					<div field="periodValidity" headerAlign="center" allowSort="true">
-						有效期(月)</div>
-					<div field="sellAmt" headerAlign="center" allowSort="true">
-						销售价格</div>
-					<div field="totalAmt" headerAlign="center" allowSort="true">
-						总价值</div>
+	</div>
 
-					<div field="salesDeductType" 
-						headerAlign="center" allowSort="true">销售提成方式</div>
-					<div field="salesDeductValue" headerAlign="center" allowSort="true">
-						销售提成值</div>
-					<div field="status" headerAlign="center"
-						allowSort="true">状态</div>
-					<div field="useRemark" headerAlign="center" allowSort="true">
-						使用说明</div>
-					<div field="remark" headerAlign="center" allowSort="true">
-						卡说明</div>
-				</div>
-			</div>
-		</div>
-	
 
 </body>
 </html>
--- Code/web/dms_repair/com.hsweb.repair/src/webcontent/js/Card/packageList.js
+++ Code/web/dms_repair/com.hsweb.repair/src/webcontent/js/Card/packageList.js
@@ -1,111 +1,121 @@
 /**
  * Created by Administrator on 2018/4/27.
  */
-var gridUrl = apiPath + repairApi + "/com.hsapi.repair.baseData.crud.queryPackage.biz.ext";
+var gridUrl = apiPath + repairApi
+		+ "/com.hsapi.repair.baseData.crud.queryPackage.biz.ext";
 var grid = null;
 var resultData = {};
-$(document).ready(function (v)
-{
-    grid  = nui.get("datagrid1");
-    grid.setUrl(gridUrl);
-    var formData = new nui.Form("#queryform").getData(false,false);
-    grid.load(formData);
+$(document).ready(function(v) {
+	grid = nui.get("datagrid1");
+	grid.setUrl(gridUrl);
+	var formData = new nui.Form("#queryform").getData(false, false);
+	grid.load(formData);
 });
-     
-//选择
+
+// 选择
 function edit() {
 	var row = grid.getSelected();
 	if (row) {
-        resultData.package1= row;
-        CloseWindow("ok");
-	      } else {
-	        nui.alert("请选择一个工时","提示");
-	 }
+		resultData.package1 = row;
+		CloseWindow("ok");
+	} else {
+		nui.alert("请选择一个工时", "提示");
+	}
 }
 
-function CloseWindow(action)
-{
+function CloseWindow(action) {
 	if (window.CloseOwnerWindow)
 		return window.CloseOwnerWindow(action);
-	else window.close();
+	else
+		window.close();
 }
 
-          //重新刷新页面
-          function refresh(){
-            var form = new  nui.Form("#queryform");
-            var json = form.getData(false,false);
-            grid.load(json);//grid查询
-            nui.get("update").enable();
-          }
+// 重新刷新页面
+function refresh() {
+	var form = new nui.Form("#queryform");
+	var json = form.getData(false, false);
+	grid.load(json);// grid查询
+	nui.get("update").enable();
+}
 
-          //查询
-          function search() {
+// 查询
+function search() {
 
-            var form = new nui.Form("#queryform");
-            var json = form.getData(false,false);
-            grid.load(json);//grid查询
-          }
+	var form = new nui.Form("#queryform");
+	var json = form.getData(false, false);
+	grid.load(json);// grid查询
+}
 
-          //重置查询条件
-          function reset(){
-            var form = new nui.Form("#queryform");//将普通form转为nui的form
-            form.reset();
-          }
+// 重置查询条件
+function reset() {
+	var form = new nui.Form("#queryform");// 将普通form转为nui的form
+	form.reset();
+}
 
-          //enter键触发查询
-          function onKeyEnter(e) {
-            search();
-          }
+// enter键触发查询
+function onKeyEnter(e) {
+	search();
+}
 
-          //当选择列时
-          function selectionChanged(){
-            var rows = grid.getSelecteds();
-            if(rows.length>1){
-              nui.get("update").disable();
-            }else{
-              nui.get("update").enable();
-            }
-          }
+// 当选择列时
+function selectionChanged() {
+	var rows = grid.getSelecteds();
+	if (rows.length > 1) {
+		nui.get("update").disable();
+	} else {
+		nui.get("update").enable();
+	}
+}
 
-          function onDrawCell(e)
-          {
-            var hash = new Array("按原价比例","按折后价比例","按产值比例","固定金额");
-            switch (e.field)
-            {
-                case "useRange":
-                    e.cellHtml = e.value==1?"连锁":"本店";
-                    break;
-                case "canModify":
-                    e.cellHtml = e.value==1?"是":"否";
-                    break;
-                case "packageRate":
-                    e.cellHtml = e.value+"%";
-                    break;
-                case "itemRate":
-                    e.cellHtml = e.value+"%";
-                    break;
-                case "partRate":
-                    e.cellHtml = e.value+"%";
-                    break;
-                case "salesDeductType":
-                    e.cellHtml = hash[e.value];
-                    break;
-                case "status":
-                e.cellHtml = e.value==1?"禁用":"启用";
-                    break; 
-                default:
-                    break;
-            }
-        }
+function onDrawCell(e) {
+	var hash = new Array("按原价比例", "按折后价比例", "按产值比例", "固定金额");
+	switch (e.field) {
+	case "useRange":
+		e.cellHtml = e.value == 1 ? "连锁" : "本店";
+		break;
+	case "isShare":
+		e.cellHtml = e.value == 1 ? "是" : "否";
+		break;
+	case "isDisabled":
+		e.cellHtml = e.value == 1 ? "禁用" : "启用";
+		break;
+	default:
+		break;
+	}
+}
+
+function getData() {
+	return resultData;
+}
+function setData(data) {
+	list = data.list || [];
+	nui.get("selectBtn").show();
+}
 
 
-        function getData()
-        {
-            return resultData;
-        }
-        function setData(data)
-        {
-            list = data.list||[];
-            nui.get("selectBtn").show();
-        }
-
+//查看详情
+function look() {
+	var row = grid.getSelected();
+	if (row) {
+		nui.open({
+			url : webPath + partDomain
+			+ "/repair/DataBase/Card/packageDetail.jsp?token=" + token,
+			title : "套餐详情",
+			width : 900,
+			height : 580,
+			onload : function() {
+				var iframe = this.getIFrameEl();
+				var data = row;
+				// 直接从页面获取，不用去后台获取
+				iframe.contentWindow.setData(data);
+			},
+			ondestroy : function(action) {
+				if (action == "saveSuccess") {
+					grid.reload();
+				}
+			}
+		});
+	} else {
+		nui.alert("请选中一条记录", "提示");
+	}
+}

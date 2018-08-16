65ecd31bb8dc56dcee4256aaa239d3a3acd0992e
faker <417652684@qq.com>
2018-08-16 11:27:13 +0800

    Base64增加 合并共享链接

---
+++ Code/api/dms_system_api/com.hsapi.system.dict/src/com_hsapi_system_dict_base64/Base64.java
@@ -0,0 +1,31 @@
+package com_hsapi_system_dict_base64;
+
+import sun.misc.BASE64Encoder;
+
+import com.eos.system.annotation.Bizlet;
+
+public class Base64 {
+	public static void main(String[] args) {
+		System.out.println("加密后的字符串：");
+	}
+
+	/* base 64加密字符串 */
+	@Bizlet("")
+	public static String jdkBase64(String str) {
+		// JDKBase64位加密
+		// 被加密的对象格式必须是 “loginName:passWord”, 例：38256par:1QAZ2WSX
+		BASE64Encoder encoder = new BASE64Encoder();
+		String encode = encoder.encode(str.getBytes());
+		return encode;
+	}
+	@Bizlet("")
+	public String lower(String c){
+		//将密码转换为小写
+		return c.toLowerCase();
+	}
+	@Bizlet("")
+	public String addStr(String a, String b){
+		//拼接字符串
+		return (a+b);
+	}
+}

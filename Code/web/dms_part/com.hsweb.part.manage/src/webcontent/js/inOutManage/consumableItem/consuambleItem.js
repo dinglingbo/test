ed128ad3e24337d1574668c23472c6f0b370867b
ZSQ <1809525631@qq.com>
2018-09-13 11:37:16 +0800

    555

--- Code/web/dms_part/com.hsweb.part.manage/src/webcontent/js/inOutManage/consumableItem/consuambleItem.js
+++ Code/web/dms_part/com.hsweb.part.manage/src/webcontent/js/inOutManage/consumableItem/consuambleItem.js
@@ -6,7 +6,7 @@ var baseUrl = window._rootUrl || "http://127.0.0.1:8080/default/";
 var partApiUrl = apiPath +  partApi + "/";
 var grid = null;
 var gridUrl = baseUrl
-		+ "com.hsapi.part.invoice.query.queryPjSellOrderMainDetailList.biz.ext";
+		+ "com.hsapi.part.purchase.repair.queryRepairOut.biz.ext";
 var queryInfoForm = null;
 var periodValidity = null;
 var partInfoUrl = partApiUrl + "com.hsapi.part.invoice.paramcrud.queryBillPartChoose.biz.ext";                             

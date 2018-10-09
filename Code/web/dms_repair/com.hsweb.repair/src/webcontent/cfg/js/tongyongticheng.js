42b331c10dae17a899ea4ed10c292b0bc5f2d5fb
ZSQ <1809525631@qq.com>
2018-10-09 11:25:32 +0800

    155

--- Code/web/dms_repair/com.hsweb.repair/src/webcontent/cfg/js/tongyongticheng.js
+++ Code/web/dms_repair/com.hsweb.repair/src/webcontent/cfg/js/tongyongticheng.js
@@ -35,15 +35,54 @@ function saveAll(){
         },
         success: function(text) {
         	grid.setUrl(gridUrl);
-        	grid.load({token:token});
+        	grid.load({params:{type:2,token:token}});
+        }
+    });
+}
+
+function savePart(){
+	var i = partGrid.getChanges("added");
+    var d = partGrid.getChanges("removed");
+	var u = partGrid.getChanges("modified");
+	for (var j = 0; j < u.length; j++) {
+		u[j].salesDeductValue = parseFloat(u[j].salesDeductValue);
+	}
+	nui.ajax({
+        url:  apiPath + repairApi+"/com.hsapi.repair.baseData.brand.saveBusinessDeduct.biz.ext",
+        type: "post",
+        cache: false,
+        async: false,
+        data: {
+        	i : i,
+        	u : u,
+        	d : d
+        },
+        success: function(text) {
+        	partGrid.setUrl(gridUrl);
+        	partGrid.load({params:{type:3,token:token}});
         }
     });
 }
 function doSearch(){
 	grid = nui.get("grid");
 	grid.setUrl(gridUrl);
-	grid.load({token:token});
+	var params={
+		type:2,
+		token: token
+	}
+	grid.load({params:params});
+	partGrid = nui.get('partGrid');
+	partGrid.setUrl(gridUrl);
+	if(partGrid){
+		var params={
+			type:3,
+			token:token
+		}
+		partGrid.load({params:params});		
+	}
 }
+
+
 //提交单元格编辑数据前激发
 function onCellCommitEdit(e) {
     var editor = e.editor;

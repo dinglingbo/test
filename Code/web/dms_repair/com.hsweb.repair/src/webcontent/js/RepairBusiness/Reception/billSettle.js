9f9b6811e7bfc70ae0a87d180e327e374ccfbb85
xiongying <1074331924@qq.com>
2018-12-18 12:12:11 +0800

    其他收款和预存冲突问题修改

--- Code/web/dms_repair/com.hsweb.repair/src/webcontent/js/RepairBusiness/Reception/billSettle.js
+++ Code/web/dms_repair/com.hsweb.repair/src/webcontent/js/RepairBusiness/Reception/billSettle.js
@@ -170,16 +170,19 @@ function setData(params){
 			var amount = 0;
 			if(params.data.ycAmt==null||params.data.ycAmt==0){
 				amount = parseFloat(params.data.mtAmt)+parseFloat(amt);
+				params.data.mtAmt = parseFloat(params.data.mtAmt)+parseFloat(amt);
 /*				params.data.mtAmt =  parseFloat(params.data.mtAmt)+parseFloat(amt);
 				params.data.mtAmt = params.data.mtAmt.toFixed(2);*/
 			}else{
 /*				params.data.mtAmt = parseFloat(params.data.mtAmt)-parseFloat(params.data.ycAmt)+parseFloat(amt);
 				params.data.mtAmt = params.data.mtAmt.toFixed(2);*/
 				amount=(parseFloat(params.data.mtAmt)-parseFloat(params.data.ycAmt)+parseFloat(amt)).toFixed(2);
+				params.data.mtAmt = parseFloat(params.data.mtAmt)+parseFloat(amt);
 			}
 			
 			netInAmt = parseFloat(amount);
-			zongAmt=parseFloat(params.data.mtAmt);
+			zongAmt=netInAmt;
+			
 			document.getElementById('totalAmt').innerHTML = "￥"+params.data.mtAmt;
 			document.getElementById('totalAmt1').innerHTML = params.data.mtAmt;
 			document.getElementById('amount').innerHTML =  netInAmt;
@@ -703,3 +706,5 @@ function CloseWindow(action) {
 	else
 		return window.close();
 }
+
+

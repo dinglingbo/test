89b27478593208510be15acadd8e9264081bd3e4
ZSQ <1809525631@qq.com>
2019-02-27 14:16:27 +0800

    修改汽配云

--- Code/web/dms_cloud_part/com.hsweb.cloud.part.purchase/src/webcontent/js/purchaseEnter/purchaseEnter.js
+++ Code/web/dms_cloud_part/com.hsweb.cloud.part.purchase/src/webcontent/js/purchaseEnter/purchaseEnter.js
@@ -660,9 +660,17 @@ function selectSupplier(elId) {
 function onRightGridDraw(e) {
     switch (e.field) {
     case "comPartBrandId":
-        if (brandHash[e.value]) {
-            e.cellHtml = brandHash[e.value].name || "";
-        } else {
+    	if(brandHash[e.value])
+        {
+//            e.cellHtml = brandHash[e.value].name||"";
+        	if(brandHash[e.value].imageUrl){
+        		
+        		e.cellHtml = "<img src='"+ brandHash[e.value].imageUrl+ "'alt='配件图片' height='25px' weight='30px'/><br> "+brandHash[e.value].name||"";
+        	}else{
+        		e.cellHtml = brandHash[e.value].name||"";
+        	}
+        }
+        else{
             e.cellHtml = "";
         }
         break;
--- Code/web/dms_cloud_part/com.hsweb.cloud.part.purchase/src/webcontent/js/sellOrderOut/pchsOrderEnterSelect.js
+++ Code/web/dms_cloud_part/com.hsweb.cloud.part.purchase/src/webcontent/js/sellOrderOut/pchsOrderEnterSelect.js
@@ -128,15 +128,30 @@ $(document).ready(function(v)
                     e.cellHtml = "";
                 }
                 break;
+//            case "partBrandId":
+//                if(partBrandIdHash[e.value])
+//                {
+//                    e.cellHtml = partBrandIdHash[e.value].name||"";
+//                }
+//                else{
+//                    e.cellHtml = "";
+//                }
+//                break;
             case "partBrandId":
                 if(partBrandIdHash[e.value])
                 {
-                    e.cellHtml = partBrandIdHash[e.value].name||"";
+//                    e.cellHtml = partBrandIdHash[e.value].name||"";
+                	if(partBrandIdHash[e.value].imageUrl){
+                		
+                		e.cellHtml = "<img src='"+ partBrandIdHash[e.value].imageUrl+ "'alt='配件图片' height='25px' weight='30px'/><br> "+partBrandIdHash[e.value].name||"";
+                	}else{
+                		e.cellHtml =partBrandIdHash[e.value].name||"";
+                	}
                 }
                 else{
                     e.cellHtml = "";
                 }
-                break;
+                break;  
             default:
                 break;
         }

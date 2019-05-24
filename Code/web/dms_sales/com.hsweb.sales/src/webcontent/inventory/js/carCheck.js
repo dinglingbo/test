34d451367ee99d408249ff40554bd0f496d810bf
xiongying <1074331924@qq.com>
2019-05-24 12:25:00 +0800

    提交验车入库

---
+++ Code/web/dms_sales/com.hsweb.sales/src/webcontent/inventory/js/carCheck.js
@@ -0,0 +1,29 @@
+var partApiUrl  = apiPath + saleApi + "/";
+var rightGridUrl=partApiUrl+"sales.inventory.queryCheckOrderDetailList.biz.ext";
+var rightGrid = null;
+$(document).ready(function(v){
+	rightGrid = nui.get("rightGrid");
+    rightGrid.setUrl(rightGridUrl);
+     rightGrid.load({
+        token:token
+    });
+});
+
+function getRow() {
+    return rightGrid.getSelected();
+}
+
+function onCancel() {
+    CloseWindow("cancel");
+}
+
+function CloseWindow(action) {
+    if (window.CloseOwnerWindow)
+        return window.CloseOwnerWindow(action);
+    else
+        window.close();
+}
+
+function choose(){
+	CloseWindow("ok");
+}

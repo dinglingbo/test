e3a270cbb5728b63dcff864707a3829e1ce54b0f
xiongying <1074331924@qq.com>
2019-06-17 18:11:39 +0800

    提交维保

--- Code/web/dms_system/com.hs.common/src/com/hs/common/QiNiuUtils.java
+++ Code/web/dms_system/com.hs.common/src/com/hs/common/QiNiuUtils.java
@@ -5,7 +5,9 @@ package com.hs.common;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -108,7 +110,7 @@ public class QiNiuUtils {
 
 	}
 	@Bizlet("获取维保大数据Authorization")
-	public static Map getMaintenanceMap(String Url) {
+	public static Map getMaintenanceMap(String Url) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		Map retMap = new HashMap();
 		
 		String envType = Env.getContributionConfig("com.vplus.login",
@@ -293,31 +295,30 @@ public class QiNiuUtils {
 		
 		}
 	 * */
-	public static String MD5(String key) {
-        char hexDigits[] = {
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-        };
-        try {
-            byte[] btInput = key.getBytes();
-            // 获得MD5摘要算法的 MessageDigest 对象
-            MessageDigest mdInst = MessageDigest.getInstance("MD5");
-            // 使用指定的字节更新摘要
-            mdInst.update(btInput);
-            // 获得密文
-            byte[] md = mdInst.digest();
-            // 把密文转换成十六进制的字符串形式
-            int j = md.length;
-            char str[] = new char[j * 2];
-            int k = 0;
-            for (int i = 0; i < j; i++) {
-                byte byte0 = md[i];
-                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                str[k++] = hexDigits[byte0 & 0xf];
-            }
-            return new String(str);
-        } catch (Exception e) {
-            return null;
-        }
+	public static String MD5(String key) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		String result = "";
+		String str = key;
+		 
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		md5.update((str).getBytes("UTF-8"));
+		byte b[] = md5.digest();
+		 
+		int i;
+		StringBuffer buf = new StringBuffer("");
+		 
+		for(int offset=0; offset<b.length; offset++){
+			i = b[offset];
+			if(i<0){
+				i+=256;
+			}
+			if(i<16){
+				buf.append("0");
+			}
+			buf.append(Integer.toHexString(i));
+		}
+		 
+		result = buf.toString();
+		return result;
     }
 	
 }
